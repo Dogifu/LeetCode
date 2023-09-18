4cf270a9920157309b397226057510095648159a
Dogifu <90715022+Dogifu@users.noreply.github.com>
2023-09-18 19:43:58 +0300

    Add files via upload

---
+++ Two_Sum/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int size, target;
+    cout << "target = ";
+    cin >> target;
+    cout << "size_of_massive = ";
+    cin >> size;
+    vector<int> mas(size);
+
+    for (int &ma: mas) {
+        cin >> ma;
+    }
+    
+    for (int i = 0; i < mas.size(); i++) {
+        for (int j = i + 1; j < mas.size(); j++) {
+            if ((mas[i] + mas[j]) == target)
+            {
+                cout << "We can get target as: "
+                <<"[" << i << "]" << " + " << "[" << j << "]";
+            }
+        }
+    }
+}
