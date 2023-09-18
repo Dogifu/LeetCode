5998f6877c3e737cf278c17e45fa414b34e472c4
Dogifu <90715022+Dogifu@users.noreply.github.com>
2023-09-18 20:15:48 +0300

    Add files via upload

--- Concatenation_of_Array/Concatenation_of_Array.cpp
+++ Concatenation_of_Array/Concatenation_of_Array.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+vector<int> getConcatenation(vector<int>& nums) {
+
+    unsigned int lenght = nums.size();
+
+    for(unsigned int i = 0; i < lenght; i++)
+    {
+        nums.push_back(nums[i]);
+    }
+    return nums;
+}
+
+int main() {
+
+    vector<int> mas_1 {1 ,2, 1};
+    getConcatenation(mas_1);
+    for (int i : mas_1)
+    {
+        cout << i << " ";
+    }
+
+}
